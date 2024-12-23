030b538edef3321adb50a28ae5d16a3c685d861b
chriscjj <chrisjohnjoy24@gmail.com>
2024-12-23 20:30:02 +0530

    Add files via upload

---
+++ Assignment2.py
@@ -0,0 +1,31 @@
+# numbers = [12, 45, 67, 23, 89]
+# print(f"List:{numbers}")
+# numbers.extend([34, 78, 56])
+# print("Updated List:", numbers)
+# for num in numbers:
+#     print(num)
+
+# person_info = {
+#     'name': 'John',
+#     'age': 25,
+#     'address': 'New York'
+# }
+# print(person_info)
+#
+# person_info['phone'] = '1234567890'
+# print(person_info)
+
+# num_set = {1, 2, 3, 4, 5}
+# print(num_set)
+#
+# num_set.add(6)
+# print(num_set)
+#
+# num_set.discard(3)
+# print(num_set)
+
+num_tuple = (1, 2, 3, 4)
+print(num_tuple)
+
+print("Length of tuple:", len(num_tuple))
+
